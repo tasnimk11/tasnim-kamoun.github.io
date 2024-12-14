6e483e6d884c7e7e79fe03fcd81175a1ce9faee0
unknown <tasnimk1108@gmail.com>
2024-12-14 20:23:55 +0100

    fix path on logo

--- src/components/navbar/LogoIcon.tsx
+++ src/components/navbar/LogoIcon.tsx
@@ -1,8 +1,10 @@
 import { Typography } from "@mui/material";
-import React from "react";
 import { NavLink } from "react-router-dom";
+import { useMenu } from "../../contexts/MenuContext";
 
 const LogoIcon = () => {
+  const { setSelectedPage } = useMenu();
+
   return (
     <Typography
       width={"10%"}
@@ -15,9 +17,9 @@ const LogoIcon = () => {
         WebkitBackgroundClip: "text", // Ensures gradient is clipped to the text
         WebkitTextFillColor: "transparent", // Makes the background visible inside the text
         textFillColor: "transparent", // Ensures compatibility with non-WebKit browsers
-
         textDecoration: "none", // Removes underline from the link
       }}
+      onClick={() => setSelectedPage("/")}
     >
       TK
     </Typography>
